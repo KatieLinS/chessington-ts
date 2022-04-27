1da26319d3c4459b821b3d16969e1beca2bf4ac3
Katie Lin <Katie.Lin@softwire.com>
2022-04-27 15:59:04 +0100

    King can move

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
+import Square from "../square";
 
 export default class King extends Piece {
     constructor(player: Player) {
@@ -8,6 +9,18 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this)
+        const availableMoves: Square[] = [];
+
+        for (let rowNum = -1; rowNum <= 1; rowNum++) {
+            for (let colNum = -1; colNum <= 1; colNum++) {
+                const availableMove = Square.at(currentSquare.row + rowNum, currentSquare.col + colNum)
+                if (availableMove.isWithinBoard() && !availableMove.equals(currentSquare)) {
+                    availableMoves.push(availableMove);
+                }
+            }
+        }
+
+        return availableMoves;
     }
 }
