7a1d77e8c82c2b11eb7dbcd083b459035e1997e8
Katie Lin <Katie.Lin@softwire.com>
2022-04-28 08:19:24 +0100

    Knight can take opponent pieces except King

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
 import Square from "../square";
+import GetAvailableOpponents from "../movement/getAvailableOpponents";
 
 export default class Knight extends Piece {
     constructor(player: Player) {
@@ -16,16 +17,23 @@ export default class Knight extends Piece {
         for (let rowNum = -2; rowNum <= 2; rowNum++) {
             if (rowNum !== 0){
                 availableMove = Square.at(currentSquare.row + rowNum, currentSquare.col + (Math.abs(rowNum) === 2 ? 1 : 2))
-                if (availableMove.isWithinBoard()) {
-                    availableMoves.push(availableMove);
-                }
+                this.checkInBoardAndEmptyThenAddMove(board, availableMoves, availableMove, currentSquare)
+
                 availableMove = Square.at(currentSquare.row + rowNum, currentSquare.col - (Math.abs(rowNum) === 2 ? 1 : 2))
-                if (availableMove.isWithinBoard()) {
-                    availableMoves.push(availableMove);
-                }
+                this.checkInBoardAndEmptyThenAddMove(board, availableMoves, availableMove, currentSquare)
             }
         }
-
         return availableMoves;
+    }
+
+    private checkInBoardAndEmptyThenAddMove(board:Board, availableMoves:Square[], availableMove:Square, currentSquare:Square) {
+        if (availableMove.isWithinBoard()) {
+            if (availableMove.isEmpty(board)){
+                availableMoves.push(availableMove);
+            } else {
+                const getAvailableOpponents = new GetAvailableOpponents()
+                getAvailableOpponents.getAvailableOpponents(board, availableMoves, availableMove, currentSquare);
+            }
+        }
     }
 }
